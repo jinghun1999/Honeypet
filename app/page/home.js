fb966fe6653b5c257b50d9e3542417cb0622906d
jinghun1999 <893884166@qq.com>
2016-11-04 13:03:07 +0800

    remove baidumap

--- app/page/home.js
+++ app/page/home.js
@@ -10,166 +10,47 @@ import {
 
 //import PureRenderMixin from 'react-addons-pure-render-mixin';
 import ViewPage from './view';
-import {
-    MapView,
-    MapTypes,
-    MapModule,
-    Geolocation
-    } from 'react-native-baidu-map';
-
+import AMapLocation from 'react-native-amap-location';
 import Dimensions from 'Dimensions';
-
-class Buttton extends Component {
-    static propTypes = {
-        label: PropTypes.string,
-        onPress: PropTypes.func
-    };
-
-    static defaultProps = {
-        label: 'Buttton',
-        onPress() {
-
-        }
-    };
-    render() {
-        return (
-            <TouchableHighlight
-                style={styles.btn}
-                onPress={this.props.onPress}>
-                <Text style={{color: 'white'}}>{this.props.label}</Text>
-            </TouchableHighlight>
-        );
-    }
-};
 
 class HomePage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            mayType: MapTypes.NORMAL,
-            zoom: 15,
-            center: {
-                longitude: 113.981718,
-                latitude: 22.542449
-            },
-            trafficEnabled: false,
-            baiduHeatMapEnabled: false,
-            markers: [{
-                longitude: 113.981718,
-                latitude: 22.542449,
-                title: "Window of the world"
-            },{
-                longitude: 113.995516,
-                latitude: 22.537642,
-                title: ""
-            }]
+            data: {},
+            dataString: null,
         };
         //this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
 
     componentDidMount() {
-        //this.fetchData(this.state.category);
+        const _this = this;
+        this.listener = AMapLocation.addEventListener((data) => {
+            _this.setState({
+                data: data,
+                dataString: JSON.stringify(data),
+            });
+        });
+        AMapLocation.startLocation({
+            accuracy: 'HighAccuracy',
+            killProcess: true,
+            needDetail: true,
+        });
+    }
+
+    componentWillUnmount() {
+        AMapLocation.stopLocation();
+        this.listener.remove();
     }
 
     render() {
         return (
-            <View style={styles.container}>
-
-
-                <View style={styles.row}>
-                    <Buttton label="Normal" onPress={() => {
-            this.setState({
-              mapType: MapTypes.NORMAL
-            });
-          }}/>
-                    <Buttton label="Satellite" onPress={() => {
-            this.setState({
-              mapType: MapTypes.SATELLITE
-            });
-          }}/>
-
-                    <Buttton label="Locate" onPress={() => {
-            Geolocation.getCurrentPosition()
-              .then(data => {
-              alert(JSON.stringify(data));
-                this.setState({
-                  zoom: 15,
-                  marker: {
-                    latitude: data.latitude,
-                    longitude: data.longitude,
-                    title: 'Your location'
-                  },
-                  center: {
-                    latitude: data.latitude,
-                    longitude: data.longitude
-                  }
-                });
-              })
-              .catch(e =>{
-                console.warn(e, 'error');
-              })
-          }}/>
-                </View>
-
-                <View style={styles.row}>
-                    <Buttton label="Zoom+" onPress={() => {
-            this.setState({
-              zoom: this.state.zoom + 1
-            });
-          }}/>
-                    <Buttton label="Zoom-" onPress={() => {
-            if(this.state.zoom > 0) {
-              this.setState({
-                zoom: this.state.zoom - 1
-              });
-            }
-
-          }}/>
-                </View>
-
-                <View style={styles.row}>
-                    <Buttton label="Traffic" onPress={() => {
-            this.setState({
-              trafficEnabled: !this.state.trafficEnabled
-            });
-          }}/>
-
-                    <Buttton label="Baidu HeatMap" onPress={() => {
-            this.setState({
-              baiduHeatMapEnabled: !this.state.baiduHeatMapEnabled
-            });
-          }}/>
-                </View>
+            <View>
+                <Text>{this.state.dataString}</Text>
             </View>
         );
     }
 }
-const styles = StyleSheet.create({
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start'
-    },
-    btn: {
-        height: 24,
-        borderRadius: 4,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#cccccc',
-        paddingLeft: 8,
-        paddingRight: 8,
-        margin: 4
-    },
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height - 200,
-        marginBottom: 16
-    }
-});
+const styles = StyleSheet.create({});
 export default HomePage;
