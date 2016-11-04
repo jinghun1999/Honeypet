71f2cf585a517e11cfa06fde61ed150b6cec329a
wqzone <wqzone@sohu.com>
2016-11-04 13:41:33 +0800

    tt

---
+++ app/page/sendrequest.js
@@ -0,0 +1,38 @@
+/**
+ * Created by User on 2016-10-26.
+ */
+'use strict';
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+    TouchableHighlight,
+    Dimensions
+} from 'react-native';
+
+const TAB_HOMEPAGE = '首页';
+const TAB_MESSAGE = '消息';
+const TAB_UC = '我的';
+
+class Index extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <text>
+                </text>
+                <text>
+                </text>
+            </View>
+        );
+    }
+}
